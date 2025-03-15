ae55e7dd85a62a1b712ae9ad4eea09cdc1d7c392
Yash Chandanshive <yashchandanshive18@gmail.com>
2025-03-15 22:33:32 +0530

    update data

--- videos.js
+++ videos.js
@@ -8,12 +8,9 @@ document.addEventListener("DOMContentLoaded", () => {
         // Create and insert the YouTube iframe player with the video ID
         const videoContainer = document.getElementById('video-container');
         videoContainer.innerHTML = `
-            <iframe
-                src="https://www.youtube.com/embed/${videoId}" 
-                frameborder="0" 
-                allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" 
-                allowfullscreen>
-            </iframe>
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/${videoId}" 
+            title="YouTube video player"
+            frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
         `;
     }
 });
